f965256226b7aa83bc6174293452b722c27062a4
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-03-30 15:54:56 +0700

    manage category - update

--- app/category/controller.js
+++ app/category/controller.js
@@ -29,5 +29,31 @@ module.exports = {
       } catch (error) {
          console.log(error)
       }
-   }
+   },
+   editView: async(req, res)=>{
+      try {
+         const { id } = req.params
+         const category = await Category.findOne({_id: id})
+
+         res.render('admin/category/edit', {
+            category
+         })
+      } catch (error) {
+         console.log(error)         
+      }
+   },
+   editAction: async(req, res)=>{
+      try {
+         const { id } = req.params
+         const { name } = req.body
+         
+         const category = await Category.findOneAndUpdate({ 
+            _id: id 
+         }, { name })
+
+         res.redirect('/category')
+      } catch (error) {
+         console.log(error)
+      }
+   },
 }
--- app/category/router.js
+++ app/category/router.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { index, createView , createAction } = require('./controller')
+const { index, createView , createAction, editView, editAction } = require('./controller')
 
 /* GET home page. */
 router.get('/', index);
 router.get('/create', createView);
 router.post('/create', createAction);
+router.get('/edit/:id', editView);
+router.put('/edit/:id', editAction);
 
 module.exports = router;
