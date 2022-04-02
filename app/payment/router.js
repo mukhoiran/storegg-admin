b79c120d8079e5feb68112043867624756bd8104
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-04-02 08:14:05 +0700

    manage payment - crud

---
+++ app/payment/router.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const { index, createView , createAction, editView, editAction, deleteAction } = require('./controller')
+
+/* GET home page. */
+router.get('/', index);
+router.get('/create', createView);
+router.post('/create', createAction);
+router.get('/edit/:id', editView);
+router.put('/edit/:id', editAction);
+router.delete('/delete/:id', deleteAction);
+
+module.exports = router;
